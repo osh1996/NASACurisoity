e9b50348f8aae2cd2efa793dfb8aabd4f5aa9420
madhephaestus <mad.hephaestus@gmail.com>
2016-07-17 15:22:35 -0400

    stub of drive engine

--- DriveEngine.groovy
+++ DriveEngine.groovy
@@ -1,1 +1,21 @@
 //Your code here
+
+return new com.neuronrobotics.sdk.addons.kinematics.IDriveEngine (){
+
+	@Override
+	public void DriveArc(MobileBase source, TransformNR newPose, double seconds) {
+		
+	}
+	@Override
+	public void DriveVelocityStraight(MobileBase source, double cmPerSecond) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void DriveVelocityArc(MobileBase source, double degreesPerSecond,
+			double cmRadius) {
+		// TODO Auto-generated method stub
+		
+	}
+}
