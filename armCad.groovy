46247ce4d8db0877acdf886eedbc054df52122ea
madhephaestus <mad.hephaestus@gmail.com>
2016-08-07 15:33:14 -0400

    stub cad generator

--- armCad.groovy
+++ armCad.groovy
@@ -1,1 +1,22 @@
-//Your code here
+
+import com.neuronrobotics.bowlerstudio.creature.ICadGenerator;
+import com.neuronrobotics.bowlerstudio.creature.CreatureLab;
+import org.apache.commons.io.IOUtils;
+import com.neuronrobotics.bowlerstudio.vitamins.*;
+import java.nio.file.Paths;
+import eu.mihosoft.vrl.v3d.FileUtil;
+import com.neuronrobotics.bowlerstudio.vitamins.*;
+println "Loading STL file"
+// Load an STL file from a git repo
+// Loading a local file also works here
+
+ICadGenerator cadGen =new ICadGenerator(){
+	@Override 
+	public ArrayList<CSG> generateCad(DHParameterKinematics d, int linkIndex) {
+		
+	}
+	@Override 
+	public ArrayList<CSG> generateBody(MobileBase b ) {return new ArrayList<>();}
+}
+
+return cadGen
