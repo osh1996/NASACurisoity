d33bcc224c2bc1b4471e7e42a845ea7e81c0e461
madhephaestus <mad.hephaestus@gmail.com>
2016-08-07 17:13:13 -0400

    moved arm

--- armCad.groovy
+++ armCad.groovy
@@ -13,15 +13,32 @@ println "Loading STL file"
 ICadGenerator cadGen =new ICadGenerator(){
 	@Override 
 	public ArrayList<CSG> generateCad(DHParameterKinematics d, int linkIndex) {
+		ArrayList<DHLink> dhLinks = d.getChain().getLinks()
 		ArrayList<CSG> parts = new ArrayList<>();
+		int i=linkIndex;
+		DHLink dh = dhLinks.get(linkIndex)
+		// Hardware to engineering units configuration
+		LinkConfiguration conf = d.getLinkConfiguration(i);
+		// Engineering units to kinematics link (limits and hardware type abstraction)
+		AbstractLink abstractLink = d.getAbstractLink(i);// Transform used by the UI to render the location of the object
+		// Transform used by the UI to render the location of the object
+		Affine manipulator = dh.getListener();
 		if(linkIndex==0){
 			File mount = ScriptingEngine.fileFromGit(
 			"https://github.com/NeuronRobotics/NASACurisoity.git",
 			"STL/swivel-bracket.STL");
 			CSG mountCSG  = Vitamins.get(mount)
+			.rotz(90)
+			.roty(90)
 			mountCSG.setManipulator(d.getRootListener());
-			
 			parts.add(mountCSG)
+			
+			File swivel = ScriptingEngine.fileFromGit(
+			"https://github.com/NeuronRobotics/NASACurisoity.git",
+			"STL/swivel.STL");
+			CSG swivelCSG  = Vitamins.get(swivel)
+			swivelCSG.setManipulator(manipulator);
+			parts.add(swivelCSG)
 		}
 		return parts
 	}
